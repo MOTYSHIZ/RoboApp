42618790f60df403bce0f54dd05acdc93ef70c06
Unknown <MOTYSHIZ@gmail.com>
2018-01-08 17:39:14 -0800

    Simple Movement on a Cube

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody rb;
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 0, moveSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-moveSpeed, 0,0, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -moveSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(moveSpeed, 0, 0, ForceMode.Acceleration);
+        }
+    }
+}
