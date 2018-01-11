a51d304bc787bd0f510abecd42dcc62b4de21753
Shiroi Kaze <shiroikaz3@gmail.com>
2018-01-11 01:01:58 -0800

    Added Camera Movement

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject target;
+    private float speed = 10.0f;
+    private Vector3 point;
+
+	// Use this for initialization
+	void Start () {
+        point = target.transform.position;
+        transform.LookAt(point);
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.LookAt(point);
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            CamRotateAround(1);
+        }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            CamRotateAround(-1);
+        }
+    }
+    public void CamRotateAround(int direction) {
+        if (direction == 1) {
+            transform.RotateAround(point, Vector3.up, 90);
+        }
+        else if (direction == -1) {
+            transform.RotateAround(point, Vector3.up, -90);
+        }
+    }
+
+}
