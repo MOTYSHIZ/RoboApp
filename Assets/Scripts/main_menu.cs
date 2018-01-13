a2b7a361f5fc41d497b09e993bbee526061f935a
Unknown <alejandraamariscal@gmail.com>
2018-01-12 17:02:49 -0800

    finished with basic UI Interface
    I have included a main menu and a return function for to move around menus

--- Assets/Scripts/main_menu.cs
+++ Assets/Scripts/main_menu.cs
@@ -10,30 +10,36 @@ public class main_menu : MonoBehaviour {
 		
 	}
 	
-	public void startGame()
+	public void startGame()                                                 //Runs level 1
     {
-        SceneManager.LoadScene("Level01", LoadSceneMode.Single);   //load main scean 
+        SceneManager.LoadScene("Level01", LoadSceneMode.Single);            //Load Level one
     }
 
-    public void Restartscean()
+    public void Restartscean()                                              //Reloads level
     {
-        SceneManager.LoadScene(Application.loadedLevel);
+        SceneManager.LoadScene(Application.loadedLevel);                    //load Restart
     }
 
-    public void GameMode()
+    public void GameMode()                                                  //brings you to Game Mode
     {
-        SceneManager.LoadScene("GameMode", LoadSceneMode.Single);
+        SceneManager.LoadScene("GameMode", LoadSceneMode.Single);           //Load Game Mode
     }
 
-    public void StoryMenu()
+    public void ChallMenu()                                                 //brings user to Challenge Menu
+    {
+        SceneManager.LoadScene("Challenge", LoadSceneMode.Single);          //Loads Challenge Menu
+    }
+
+    public void GoBack()                                                    // Able to return to menu
+    {
+        SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);          //Bring you to the Main Menu
+    }
+
+    public  void StoryMode()
     {
         SceneManager.LoadScene("Story", LoadSceneMode.Single);
     }
 
-    public void ChallMenu()
-    {
-        SceneManager.LoadScene("Challenge", LoadSceneMode.Single);
-    }
 }
 
   
