186da4f3b3ebe2a5e6cb1877837911eb37dc220a
Unknown <MOTYSHIZ@gmail.com>
2018-01-18 12:39:59 -0800

    Command Queue Functionality and UI
    Command Queue now works, additionally, it scrolls if there are too many commands.

---
+++ Assets/Scripts/CommandQVisual.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandQVisual : MonoBehaviour {
+
+    public List<GameObject> commandIcons;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    public void addIconVisual (int type) {
+        GameObject icon = Instantiate(commandIcons[type]);
+        
+        icon.transform.SetParent(gameObject.transform, false);
+        //icon.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    public void removeFirstIconVisual()
+    {
+        Destroy(gameObject.transform.GetChild(0).gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementCQ.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementCQ : PlayerMovement
+{
+    LinkedList<int> commandLinkedList = new LinkedList<int>();
+    public CommandQVisual commandQVisual;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            moveForward();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            turnLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveBackward();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            turnRight();
+        }
+    }
+    
+    public void queueMove(int commandType)
+    {
+        commandLinkedList.AddLast(new LinkedListNode<int>(commandType));
+        commandQVisual.addIconVisual(commandType);
+    }
+
+    public void runCommand()
+    {
+        StartCoroutine("RunCommandList");
+    }
+
+    IEnumerator RunCommandList()
+    {
+        while(commandLinkedList.Count > 0)
+        {
+            int currentCommand = commandLinkedList.First.Value;
+            commandLinkedList.RemoveFirst();
+            commandQVisual.removeFirstIconVisual();
+
+            if (currentCommand == 0)
+            {
+                moveForward();
+            }
+            if (currentCommand == 1)
+            {
+                moveBackward();
+            }
+            if (currentCommand == 2)
+            {
+                turnLeft();
+            }
+            if (currentCommand == 3)
+            {
+                turnRight();
+            }
+
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+}
