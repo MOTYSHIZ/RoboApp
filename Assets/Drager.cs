75466782675a0fecb1c94c5399bcfab38450dbfc
Bassam Elsaleh <cpvpandrew@gmail.com>
2018-01-19 16:51:27 -0800

    working on drag and drop

---
+++ Assets/Drager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    public static GameObject itemDrag;
+    Vector3 startp;    Transform startPar;
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemDrag = gameObject;
+        startp = transform.position;
+        startPar = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemDrag = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent != startPar)
+        {
+            transform.position = startp;
+        }
+    }
+}
---
+++ Assets/slotCon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class slotCon : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount>0)
+            {
+                return transform.GetChild (0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public object itemDrag { get; private set; }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            //DragHandler.itemDrag.transform.SetPartent(transform); 
+        }
+    }
+}
