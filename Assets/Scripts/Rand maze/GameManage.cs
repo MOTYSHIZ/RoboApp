03c6a23f27cb6da9cdd73061b7ed3c2e8442e886
Bassam Elsaleh <cpvpandrew@gmail.com>
2018-02-02 17:57:24 -0800

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Rand maze/GameManage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManage : MonoBehaviour {
+
+    private void Start()
+    {
+        BeginGame();                                // start game
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))      //when the space bare is pressed then the game Restarts
+        {
+            RestartGame();
+        }
+    }
+
+    public Maze mazePrefab;                 //prefab for the maze
+
+    private Maze mazeInstance;              //
+
+    private void BeginGame()                // when the maze starts there will be a randomly generated maze avaliable
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+       StartCoroutine (mazeInstance.Generate());
+    }
+
+    private void RestartGame()              // when the game restarts, it should destroy the last maze then go back to begingame
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
